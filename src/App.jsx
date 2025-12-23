0584f9ebc1249c556c8bda33a1b115fada4ba24d
Oghenefegor Idoro <69634999+fegig@users.noreply.github.com>
2025-12-23 20:27:20 +0100

    init repo for movie-thrillers

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Tests from './components/tests'
+import React from 'react'
+
+
+function App() {
+
+  return (
+    <React.Fragment>
+
+      <Tests name="John" age="20" email="john@gmail.com" />
+    </React.Fragment>
+  )
+}
+
+export default App
